c1b62b84e000504b537834a8498e626c29f97079
Nick <nboober@terpmail.umd.edu>
2019-09-26 17:52:02 -0400

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+    def change
+        create_table :haunted_houses do |h|
+            h.string :name
+            h.string :location
+            h.string :theme
+            h.integer :price
+            h.boolean :family_friendly
+            h.string :opening_date
+            h.string :closing_date
+            h.string :description
+        end
+    end
+end
